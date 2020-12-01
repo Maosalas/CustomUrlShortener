72a21a6e4a70a05a72dd11a61cd573ae96bb6aea
Mauricio Salas <mao.salas.steller@gmail.com>
2020-11-30 18:02:21 -0600

    projects files

---
+++ connection.py
@@ -0,0 +1,8 @@
+import mysql.connector
+import config
+mydb = mysql.connector.connect(
+    host=config.host,
+    user=config.user,
+    password=config.password,
+    database=config.database
+)
---
+++ main.py
@@ -0,0 +1,64 @@
+from builtins import print
+
+import connection
+import sys
+import time
+import random
+import string
+
+cursor = connection.mydb.cursor()
+
+
+def main():
+    print("********MSS Url Shortener********")
+    respuesta = input(""" 
+    A: Acortar una nueva dirreción
+    B: Ver todas nuestras dirreciones
+    C: Salir """)
+    if respuesta == "A" or respuesta == "a":
+        acortar()
+    elif respuesta == "B" or respuesta == "b":
+        fetchUrls()
+    elif respuesta == "C" or respuesta == "c":
+        sys.exit
+    else:
+        print("Debes de ingresar un dato valido entre A y C")
+        print("Porfavor ingresa de nuevo")
+        main()
+
+
+def acortar():
+    urlOriginal = input("Ingrese el url que desea acortar: ")
+    urlShort = generarRandomUrlId()
+    cursor.execute("""INSERT INTO urls (urlOriginal,urlCorto) values (%s,%s)""", (urlOriginal, urlShort,))
+    connection.mydb.commit()
+    print("Pagina acortada a:" + " https://www.tikosolutions.com/urls?url=" + urlShort)
+
+def fetchUrls():
+    cursor.execute("SELECT * FROM urls")
+    result = cursor.fetchall()
+    for x in result:
+        print(" https://www.tikosolutions.com/urls?url=" + x[2])
+    time.sleep(3)
+    print(chr(27) + '[2j')
+    main()
+
+
+def generarRandomUrlId():
+    letras = string.ascii_lowercase
+    result_str = ''.join(random.choice(letras) for i in range(6))
+    validate = validateUrlIdDB("mss-" + result_str)
+    if validate == 1:
+        generarRandomUrlId()
+    else:
+        return "mss-" + result_str
+
+
+def validateUrlIdDB(randomUrl):
+    cursor.execute("""SELECT COUNT(urlCorto) FROM urls WHERE urlCorto= %s""", (randomUrl,))
+    result = cursor.fetchone()
+    return result[0]
+
+
+if __name__ == '__main__':
+    main()
